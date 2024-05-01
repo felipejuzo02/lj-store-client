ddb2e4567540aca6d20133ac610e81e676bd8dff
felipejuzo02 <felipejuzo@hotmail.com>
2024-04-30 23:19:17 -0300

    feat: insert vue3-toastify

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import { createApp } from 'vue'
 import './styles/style.css'
 import App from './App.vue'
 import router from './routes/index'
+import Vue3Toasity from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 createApp(App)
   .use(router)
+  .use(
+    Vue3Toasity,
+    {
+      autoClose: 3000,
+      pauseOnFocusLoss: false,
+      theme: 'dark'
+    }
+  )
   .mount('#app')
